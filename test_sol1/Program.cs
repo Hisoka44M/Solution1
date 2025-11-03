4a216e2d314b8f6bdbcd8f94b9893a0b070b8bab
Gorbunova Olga <ogorbunova249@gmail.com>
2025-11-03 21:51:11 +0500

    Добавьте файлы проекта.

---
+++ test_sol1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace test_sol1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Мини-калькулятор ===");
+
+            Console.Write("Введите первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Сложение: {a} + {b} = {a + b}");
+            Console.WriteLine($"Вычитание: {a} - {b} = {a - b}");
+            Console.WriteLine($"Умножение: {a} × {b} = {a * b}");
+
+            if (b != 0)
+                Console.WriteLine($"Деление: {a} ÷ {b} = {a / b}");
+            else
+                Console.WriteLine("Деление на ноль невозможно!");
+        }
+    }
+}
